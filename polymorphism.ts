a1087e5a041cc2d60c556977d75549b356651950
Ebrahim Salehipanah <e.salehipanah@gmail.com>
2022-03-01 20:40:50 +0330

    polymorphism

---
+++ polymorphism.ts
@@ -0,0 +1,5 @@
+/*
+ * 1. method / param overriding -> child overrides parent method
+ * 2. method overloading -> same name methods (javascript doesn't support)
+ * 3. interfaces / abstract classes implements
+*/
